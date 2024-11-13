77981c517b5fdb8259f24cc0b94d0bfc82e5ed1c
lailixuann <lailixuan925@gmail.com>
2024-11-13 14:45:47 +0800

    Testing Challenge B

--- Challenge B.py
+++ Challenge B.py
@@ -5,8 +5,35 @@ Create a program that will read the generated file above and print to the consol
 its type. Spaces before and after the alphanumeric object must be stripped.
 """
 
+def identify_obj_type(obj):
+    obj = obj.strip()
+
+    if obj.isalpha():
+        return "Alphabetical String"
+    
+    elif obj.isdigit():
+        return "Integer"
+    
+    try:
+        float(obj)
+        return "Real Number" if '.' in obj else "Integer"
+    
+    except ValueError:
+        return "Alphanumeric" if obj.isalnum() else "Unknown"
+
+    
+
 def read_file(filename):
     with open(filename, 'r') as file:
         content = file.read()
+
+    obj = content.split(',')
+
+    for i in obj[:5]:
+        obj_type = identify_obj_type(i)
+        print(f"Object: '{i}', Type: {obj_type}")
+
+
+read_file('output.txt')
     
     
